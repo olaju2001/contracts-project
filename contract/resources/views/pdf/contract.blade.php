f1df29d80b7db4cc1f17f9700849b8f1566b6937
hrr2000 <thedragonea@gmail.com>
2023-06-29 22:28:06 +0300

    try direction

--- contract/resources/views/pdf/contract.blade.php
+++ contract/resources/views/pdf/contract.blade.php
@@ -6,7 +6,9 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
-        *{ font-family: DejaVu Sans, sans-serif;direction: rtl; }
+      @if(app()->getLocale() == 'ar')
+        *{ font-family: DejaVu Sans, sans-serif;direction: rtl; text-align: right; }
+      @endif
         .table{
             width: 100%;
         }
