b7eeaef4887cd880c6130d2b3b98ca693331ab9b
hrr2000 <thedragonea@gmail.com>
2023-06-29 19:00:12 +0300

    test

--- contract/app/Http/Controllers/Api/ContractController.php
+++ contract/app/Http/Controllers/Api/ContractController.php
@@ -109,7 +109,7 @@ class ContractController extends Controller
      */
     public function create(ContractRequest $request)
     {
-        echo app()->getLocale();
+        dd(app()->getLocale());
         // please make that in validation
         $contractCheck = $this->contractModel->where('user_id',Auth::id())->first();
 
