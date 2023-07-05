b99553ce06151e088da4e2e9e093c96ad92f9f5e
hrr2000 <thedragonea@gmail.com>
2023-07-05 09:25:10 +0300

    test the font usage

--- contract/resources/views/pdf/contract.blade.php
+++ contract/resources/views/pdf/contract.blade.php
@@ -7,12 +7,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
       @font-face {
-        font-family: "CustomFont";
+        font-family: "DINNEXTLTArabic";
         src: url("../fonts/DINNextLTArabic-Regular-3.ttf") format("truetype");
       }
       @if(app()->getLocale() == 'ar')
         *{ 
-          font-family: 'Droid Arabic Naskh', Arial, sans-serif;
+          font-family: "DINNEXTLTArabic", sans-serif;
           direction: rtl;
          }
       @endif
